eccae10f572cba650d5a27019464922f5009f395
Bharati Gogoi <bharatigogoi@Bharatis-MacBook-Air.local>
2025-06-25 00:51:08 +0530

    feat: [Toast, IconButton] - created IconButton component. - created Toast component with auto close and manual close.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,36 @@ import Toast from "./components/Toast";
 
 function App() {
   return (
-    <div className="p-6 bg-gray-100">
-      <Toast title="Title" variant="info" />
+    <div className="flex flex-col gap-2 p-4">
+      <p>Toasts</p>
+      <Toast
+        title="Success!"
+        variant="error"
+          autoClose
+        description="File uploaded successfully."
+        closable
+        transitionDirection="right"
+      />
+      <Toast
+        title="Success! This title is a bit very very long to test ui compatibility"
+        variant="info"
+        description="This title is a bit very very long to test ui compatibility"
+        closable
+        transitionDirection="left"
+      />
+      <Toast
+        title="Success!"
+        variant="success"
+        description="Data saved successfully."
+        closable
+      />
+      <Toast
+        title="Success!"
+        variant="warning"
+        description="File uploaded successfully."
+        closable
+        transitionDirection="left"
+      />
     </div>
   );
 }
--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,44 +1,138 @@
+import { useEffect, useState } from "react";
+import CloseIcon from "../../assets/icons/close.svg?react";
+import ErrorIcon from "../../assets/icons/error.svg?react";
+import InfoIcon from "../../assets/icons/info.svg?react";
+import SuccessIcon from "../../assets/icons/success.svg?react";
+import WarningIcon from "../../assets/icons/warning.svg?react";
+import IconButton from "../IconButton";
+
 type ToastVariant = "info" | "success" | "warning" | "error";
+type ToastDirection = "left" | "right";
 
 interface IToastProps {
   title: string;
   variant: ToastVariant;
+  autoClose?: boolean;
   closable?: boolean;
   description?: string;
   duration?: number;
+  onClose?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  transitionDirection?: ToastDirection;
 }
+
+const getToastColor = (variant: ToastVariant): string => {
+  const colorMap = {
+    error: "#FFE2E5",
+    info: "#F7FAFC",
+    success: "#E7F4E8",
+    warning: "#FFF4E4",
+  };
+  return colorMap[variant];
+};
 
 function ToastIcon(variant: ToastVariant) {
   switch (variant) {
     case "error": {
-      return <>!</>;
+      return <ErrorIcon />;
     }
     case "info": {
-      return <>!</>;
+      return <InfoIcon />;
     }
     case "success": {
-      return <>!</>;
+      return <SuccessIcon />;
     }
     case "warning": {
-      return <>!</>;
+      return <WarningIcon />;
     }
     default: {
-      return <>def</>;
+      return <></>;
     }
   }
 }
 
-const Toast = ({ title, variant, closable, description }: IToastProps) => {
+const Toast = ({
+  title,
+  variant,
+  autoClose,
+  closable,
+  description,
+  duration = 5000,
+  onClose,
+  transitionDirection,
+}: IToastProps) => {
+  const [showToast, setShowToast] = useState(true);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    setIsAnimating(true);
+  }, []);
+
+  useEffect(() => {
+    if (!autoClose) return;
+
+    const timer = setTimeout(() => {
+      handleClose();
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration]);
+
+  function handleClose(e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    setIsAnimating(false);
+
+    setTimeout(() => {
+      setShowToast(false);
+      if (e) onClose?.(e);
+    }, 500);
+  }
+
+  function getTransitionDirection(direction?: ToastDirection) {
+    switch (direction) {
+      case "left": {
+        return isAnimating
+          ? "translate-x-0 opacity-100"
+          : "-translate-x-full opacity-0";
+      }
+      case "right": {
+        return isAnimating
+          ? "translate-x-0 opacity-100"
+          : "translate-x-full opacity-0";
+      }
+      default: {
+        return isAnimating
+          ? "translate-x-0 opacity-100"
+          : "translate-x-full opacity-0";
+      }
+    }
+  }
+
+  if (!showToast) return null;
+
   return (
-    <div className="border-r-4 bg-amber-500 flex flex-row items-center gap-1 rounded">
+    <div
+      className={`flex flex-row items-center gap-3 rounded p-3 h-[45px] w-3xs transition-all duration-500 ease-in-out transform ${
+        getTransitionDirection(transitionDirection)
+        // isAnimating ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
+      }`}
+      style={{ backgroundColor: getToastColor(variant) }}
+      id="toast"
+    >
       {ToastIcon(variant)}
-
-      <div>
-        <p>{title}</p>
-        {description && <p>{description}</p>}
+      <div className="flex flex-col">
+        <p className="text-xs font-bold font-inter truncate max-w-[10ch]">
+          {title}
+        </p>
+        {description && (
+          <p className="text-xs font-normal truncate max-w-[20ch]">
+            {description}
+          </p>
+        )}
       </div>
-
-      {closable && <button>x</button>}
+      {closable && (
+        <IconButton onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+      )}
     </div>
   );
 };
